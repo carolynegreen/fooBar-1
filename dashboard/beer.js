87dd4e6a912d9e812086fb8ca259319c5758dde6
carolynegreen <carolynemarie94@gmail.com>
2021-12-03 14:14:53 +0100

    restdb added

--- dashboard/beer.js
+++ dashboard/beer.js
@@ -1,12 +1,31 @@
 "use strict";
 
-loadData();
+fetch("https://kea2sem-4cc6.restdb.io/rest/foobar", {
+  method: "GET",
+  headers: {
+    "x-apikey": "61aa0b02c7048f219d10f8de",
+  },
+})
+  .then((res) => res.json())
+  .then((response) => {
+    showProduct(response);
+  })
+  .catch((err) => {
+    console.error(err);
+  });
 
-async function loadData() {
-  const response = await fetch("https://foo-bar-3.herokuapp.com/");
-  const data = await response.json();
-  console.log(data);
-  beerTaps();
+function showProduct(product) {
+  console.log(product);
+  //data.comments
+  //grab the template
+  const template = document.querySelector(".single-product-template").content;
+  //loop through data.comments
+  product.forEach((product) => {
+    console.log(product);
+    const copy = template.cloneNode(true);
+    copy.querySelector("img").src = `${product.BeerImg}`;
+    copy.querySelector("h2").textContent = `${product.BeerName}`;
+    copy.querySelector("p").textContent = product.BeerDescription;
+    document.querySelector("section").appendChild(copy);
+  });
 }
-
-function beerTaps() {}
