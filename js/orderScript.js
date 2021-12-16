70ec2e2f2eb516383101e9550f9ed82e815b9335
siragabari <sira0245@stud.kea.dk>
2021-12-16 14:04:22 +0100

    animation js

--- js/orderScript.js
+++ js/orderScript.js
@@ -412,21 +412,3 @@ function post(order) {
     document.getElementById("statusForm").style.display = "block";
     document.getElementById("status").style.display = "none";
 }
-
-
-/**
- * ANIMATIONS
- */
-
-function waitingAnimation() {
-    document.querySelector(".waiting").style.display = "block";
-    document.querySelector(".waiting").style.pointerEvents = "all";
-    document.querySelector(".beerIcon").classList.add("beerAni");
-    setTimeout(function() {
-        document.querySelector(".waiting").style.display = "none";
-        document.querySelector(".waiting").style.pointerEvents = "none";
-        document.querySelector(".beerIcon").classList.remove("beerAni");
-    }, 6000);
-}
-
-
